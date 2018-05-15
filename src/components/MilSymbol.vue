5209694c29b8eb096626a691079711a7a2d365ae
Kjell Magne Fauske <kjellmf@gmail.com>
2018-05-15 21:00:06 +0200

    Create basic project from template

---
+++ src/components/MilSymbol.vue
@@ -0,0 +1,56 @@
+<template>
+</template>
+
+<script>
+import ms from "milsymbol";
+import Vue from "vue";
+
+export default {
+  name: "MilSymbol",
+  props: {
+    sidc: String,
+    size: {
+      type: Number,
+      default: 15
+    },
+    modifiers: {
+      type: Object
+    }
+  },
+
+  mounted() {
+    this.setSymbol();
+  },
+
+  methods: {
+    setSymbol() {
+      let symb = new ms.Symbol(
+        this.sidc,
+        { size: this.size, simpleStatusModifier: true },
+        this.modifiers || {}
+      );
+      this.$el.innerHTML = symb.asSVG();
+    }
+  },
+
+  watch: {
+    sidc: function(v) {
+      this.setSymbol();
+    },
+    size: function(v) {
+      this.setSymbol();
+    }
+  },
+
+  render(h) {
+    return h("span", { attrs: { class: "milsymbol" } });
+  }
+};
+
+// This is how you can change the frame fill color:
+// var cm = ms.getColorMode("Light");
+// cm.Friend = cm.Neutral;
+// var symb = new ms.Symbol(sidc, {colorMode:cm});
+// symb.setOptions({fillColor: "rgb(255,0,0)"})
+</script>
+
--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import Vuetify from 'vuetify';
 
-Vue.config.productionTip = false
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
