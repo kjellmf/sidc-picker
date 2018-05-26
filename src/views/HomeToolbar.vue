1dbd23a7bbf0c7e73b5e5cd29d1279f9da3dae19
Kjell Magne Fauske <kjellmf@gmail.com>
2018-05-26 14:41:55 +0200

    Add buttons for downloading as PNG and SVG
    Does not work in Edge.

--- src/views/HomeToolbar.vue
+++ src/views/HomeToolbar.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
-    <v-btn @click="doCopy" v-shortkey="['alt', 'c']" @shortkey="doCopy" icon title="Copy SIDC to clipboard">
+    <v-btn @click="doCopy" class="mx-0" v-shortkey="['alt', 'c']" @shortkey="doCopy" icon title="Copy SIDC to clipboard">
       <v-icon>assignment</v-icon>
     </v-btn>
-    <v-btn icon title="Clear" @click="clear" v-shortkey="['alt', 'x']" @shortkey="clear">
+    <v-btn class="mx-0" icon title="Clear" @click="clear" v-shortkey="['alt', 'x']" @shortkey="clear">
       <v-icon>clear</v-icon>
+    </v-btn>
+    <v-btn class="mx-0 px-0" flat title="Download symbol as PNG" @click="downloadPNG" href="download" download="nn.png">
+      <v-icon>save_alt</v-icon>&nbsp; PNG
+    </v-btn>
+    <v-btn class="mx-0 px-0"  flat title="Download symbol as SVG" @click="downloadSVG" href="download" download="nn.svg">
+      <v-icon>save_alt</v-icon>&nbsp; SVG
     </v-btn>
   </div>
 
@@ -12,10 +18,12 @@
 
 <script>
 import { Sidc } from "../symbology/sidc";
+import * as ms from "milsymbol";
 
 export default {
   name: "HomeToolbar",
   data: () => ({
+    
   }),
   computed: {
     sidc: {
@@ -33,7 +41,7 @@ export default {
       this.$copyText(this.sidc)
         .then(e => {
           console.log("Dispatch");
-          this.$store.dispatch("showMessage", "SIDC copied to clipboard")
+          this.$store.dispatch("showMessage", "SIDC copied to clipboard");
         })
         .catch(e => {
           console.warning("Failed to copy SIDC to clipboard");
@@ -44,12 +52,24 @@ export default {
       let oldSIDC = new Sidc(this.sidc);
       oldSIDC.hqtfd = "0";
       oldSIDC.amplifier = "0";
-      oldSIDC.amplifierDescriptor = "0"
-      
+      oldSIDC.amplifierDescriptor = "0";
+
       this.sidc = oldSIDC.toString();
-      
+    },
+
+    downloadPNG(ev) {
+      let downloadSymbol = new ms.Symbol(this.sidc);
+      let a = ev.currentTarget;
+      a.setAttribute("href", downloadSymbol.asCanvas().toDataURL())
+      a.setAttribute("download", this.sidc + ".png");
+    },
+
+    downloadSVG(ev) {
+      let downloadSymbol = new ms.Symbol(this.sidc);
+      let a = ev.currentTarget;
+      a.setAttribute("href", downloadSymbol.toDataURL())
+      a.setAttribute("download", this.sidc + ".svg");
     }
-    
   }
 };
 </script>
