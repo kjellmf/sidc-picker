165bf4b0145725142d1d4f7af1a54f8387102533
Kjell Magne Fauske <kjellmf@gmail.com>
2018-07-07 16:38:01 +0200

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/recommended',
+    '@vue/airbnb'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    quotes: 0,
+    'object-curly-spacing': [1, "never"]
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+};
